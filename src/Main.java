64e6c27f9ec9a035bc3edaef265f63f493557731
ZhdanovDM <gdanovdm@gmail.com>
2023-04-11 23:22:12 +0500

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         List<Integer> nums2 = new ArrayList<>(List.of(1, 2, 5, 5, 7, 7, 3, 3, 6, 4));
